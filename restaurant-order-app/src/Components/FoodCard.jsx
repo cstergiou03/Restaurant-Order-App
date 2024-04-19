d4bb1ea92ffac8be0be7e847bc507f7141787e35
CHRISTODOULOS STERGIOU <96772662+cstergiou03@users.noreply.github.com>
2024-04-19 21:13:30 +0300

    deleted:    restaurant-order-app/README.md 	modified:   restaurant-order-app/package-lock.json 	modified:   restaurant-order-app/package.json 	modified:   restaurant-order-app/src/Components/FoodCard.jsx 	modified:   restaurant-order-app/src/Style/foodCard.css

--- restaurant-order-app/src/Components/FoodCard.jsx
+++ restaurant-order-app/src/Components/FoodCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import "../Style/foodCard.css";
 import Cards from "./Cards";
 import Payment from "./Payment";
+import Popup from 'reactjs-popup';
 
 function FoodCard(){
 
@@ -15,16 +16,50 @@ function FoodCard(){
 
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedItems, setSelectedItems] = useState([]);
+    const [username, setUsername] = useState("Chris");
+    const [surname, setSurname] = useState("Stergiou");
+    const [address, setAddress] = useState("Trapezountos 1");
+    const [phoneNumber, setPhoneNumber] = useState("6987748580");
 
-    // Λειτουργία φιλτραρίσματος βάσει του searchTerm
     const filteredFoods = foods.filter(food => {
         return food.name.toLowerCase().includes(searchTerm.toLowerCase());
     });
 
+    function handleUsername(event){
+        setUsername(event.target.value);
+    }
+
+    function handleSurname(event){
+        setSurname(event.target.value);
+    }
+
+    function handleAddress(event){
+        setAddress(event.target.value);
+    }
+
+    function handlePhoneNumber(event){
+        setPhoneNumber(event.target.value);
+    }
+
     return(
         <div className="foodCard-container">
             <div className="top-container">
-                <img src="../src/assets/user.jpg" className="user-img" alt="User"></img>
+                <Popup className="popup" trigger=
+                    {<button className='img-button'><img src="../src/assets/user.jpg" className="user-img" alt="User"></img></button>} 
+                    modal nested>
+                    {
+                        close => (
+                            <div className='modal'>
+                                <h2 className='info-head'>Customer Info</h2>
+                                <input type="text" placeholder='Name' className='info' value={username} onChange={handleUsername}/>
+                                <input type="text" placeholder='Surname' className='info' value={surname} onChange={handleSurname}/>
+                                <input type="text" placeholder='Address' className='info' value={address} onChange={handleAddress}/>
+                                <input type="text" placeholder='Phone Number' className='info' maxLength={10} value={phoneNumber} onChange={handlePhoneNumber}/>
+                                <button className='info-button' onClick={() => close()}>Close</button>
+                            </div>
+                        )
+                    }
+                </Popup>   
                 <input 
                     type="text"
                     placeholder="Search..."
